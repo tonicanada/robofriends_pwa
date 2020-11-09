2c2bb3f541f83eca1e4733dafc71ffd74deef467
tonicanada <toni.cm@gmail.com>
2020-11-08 22:56:49 -0300

    updated service worker

--- src/index.js
+++ src/index.js
@@ -8,14 +8,13 @@ import "./index.css";
 import App from "./containers/App";
 import { searchRobots, requestRobots } from "./reducers";
 import tachyons from "tachyons";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware)
-);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -24,3 +23,13 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
